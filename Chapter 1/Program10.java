cb2ec583b257fb7dcc75f1545408460d612aadfb
Lumos059 <2547605098@qq.com>
2024-09-21 14:55:01 +0800

    Add files via upload

---
+++ Chapter 1/Program10.java
@@ -0,0 +1,8 @@
+public class Program10 {
+    public static void main(String[] args) {
+        double time = 45.5 / 60;
+        double mile = 14 / 1.6;
+        double average_speed = mile / time;
+        System.out.println(average_speed);
+    }
+}
---
+++ Chapter 1/Program8.java
@@ -0,0 +1,9 @@
+public class Program8 {
+    public static void main(String[] args) {
+        double radius = 5.5;
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+        System.out.println("perimeter = " + perimeter);
+        System.out.println("area = " + area);
+    }
+}
