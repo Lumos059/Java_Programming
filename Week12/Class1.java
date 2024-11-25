20a4765b9a9846f14399f3151266fceb3fefa489
Lumos059 <2547605098@qq.com>
2024-11-25 14:34:35 +0800

    Add files via upload

---
+++ Week12/Class1.java
@@ -0,0 +1,13 @@
+//Example 9.3
+package package1;
+public class Class1 {
+    public int x = 5;
+    int y = 10;
+    void m1(){
+        System.out.println(x);
+        System.out.println(y);
+    }
+    public static void main(String[] args){
+        System.out.println("This is main method of class-1");
+    }
+}
